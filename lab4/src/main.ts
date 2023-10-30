5db45a4f136b10627cb2f95e94e8b36f03b1dfbf
hubcio2115 <kowalski.hubercik1@gmail.com>
2023-10-30 16:24:15 +0100

    deleted vue-query

--- lab4/src/main.ts
+++ lab4/src/main.ts
@@ -1,11 +1,8 @@
-import { VueQueryPlugin } from "@tanstack/vue-query";
 import { createApp } from "vue";
 
 import App from "./App.vue";
 import "./main.css";
 
 const app = createApp(App);
-
-app.use(VueQueryPlugin);
 
 app.mount("#app");
